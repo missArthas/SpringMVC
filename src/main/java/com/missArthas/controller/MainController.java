cb69ed2be4b964c13571f476b9a65b8dea0eb73c
missArthas <forreg126@163.com>
2018-05-21 12:44:21 +0800

    web

--- src/main/java/com/missArthas/controller/MainController.java
+++ src/main/java/com/missArthas/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.missArthas.controller;
 
+import com.missArthas.config.ConfigTest;
+import com.missArthas.config.TransportClientConfig;
 import com.missArthas.entity.UserEntity;
 import com.missArthas.service.AuthorityService;
 import com.missArthas.service.UserService;
+import org.elasticsearch.search.SearchHit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,9 +20,19 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.search.SearchHits;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.net.UnknownServiceException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
 
 /**
  * Created by shhuang on 2016/12/28.
@@ -76,29 +89,86 @@ public class MainController {
 
     @RequestMapping(value = "/corpus",method = RequestMethod.GET)
     public String corpus() throws UnknownHostException {
+//        Settings settings = Settings.settingsBuilder()
+//                .put("cluster.name", "parallel-corpus")
+//                .put("client.transport.sniff", true).build();
+//
+//        TransportClient client = TransportClient.builder().settings(settings).build()
+//                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), Integer.parseInt("9300")));
 
-        Settings settings = Settings.settingsBuilder()
-                .put("cluster.name", "parallel-corpus")
-                .put("client.transport.sniff", true).build();
-
-        TransportClient client = TransportClient.builder().settings(settings).build()
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), Integer.parseInt("9300")));
-
-        SearchResponse response = client.prepareSearch("corpus")
+        SearchResponse response = transportClient.prepareSearch("corpus")
                 .setTypes("pair")
-                .setQuery(QueryBuilders.termQuery("chinese", "中国"))
+                .setQuery(QueryBuilders.termQuery("chinese", "他们"))
                 .execute()
                 .actionGet();
         SearchHits searchHits = response.getHits();
-        System.out.println("-----------------在["+"chinese"+"]中搜索关键字["+"中国"+"]---------------------");
+        System.out.println("-----------------在["+"chinese"+"]中搜索关键字["+"他们"+"]---------------------");
         System.out.println("共匹配到:"+searchHits.getTotalHits()+"条记录!");
         return "corpus/index";
     }
 
     @RequestMapping(value = "/search",method = RequestMethod.POST)
-    public String search(Model model){
-        model.addAttribute("pair", "this is the pair");
-        return "corpus/index";
+    @ResponseBody
+    public ModelAndView search(HttpServletRequest request){
+        ModelAndView mv = new ModelAndView();
+
+        try {
+            request.setCharacterEncoding("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        String keyword=request.getParameter("keyword");
+        String language=request.getParameter("language");
+        SearchResponse response = transportClient.prepareSearch("corpus")
+                .setTypes("pair")
+                .setQuery(QueryBuilders.termQuery(language, keyword))
+                .setSize(50)
+                .setFrom(10)
+                .execute()
+                .actionGet();
+
+        SearchHits searchHits = response.getHits();
+        List list = new ArrayList<Map<String, Object>>();
+
+        for(SearchHit hit:searchHits){
+            Map<String, Object> t = hit.getSource();
+            list.add(t);
+            System.out.println(t);
+        }
+
+
+        mv.addObject("totalRecords", searchHits.getTotalHits());
+        mv.addObject("keyword", keyword);
+        mv.addObject("hits", list);
+        mv.addObject("language", language);
+        mv.setViewName("corpus/index");
+        return mv;
+    }
+
+    @RequestMapping(value = "/searchtest",method = RequestMethod.GET,produces="application/json")
+    @ResponseBody
+    public String searchtest(String keyword, String language, int size, int from){
+
+
+        SearchResponse response = transportClient.prepareSearch("corpus")
+                .setTypes("pair")
+                .setQuery(QueryBuilders.termQuery(language, keyword))
+                .setSize(size)
+                .setFrom(from)
+                .execute()
+                .actionGet();
+
+        SearchHits searchHits = response.getHits();
+        List list = new ArrayList<Map<String, Object>>();
+
+        for(SearchHit hit:searchHits){
+            Map<String, Object> t = hit.getSource();
+            list.add(t);
+        }
+
+
+        return JSON.toJSONString(list);
     }
 
 
